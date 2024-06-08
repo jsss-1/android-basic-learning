83d18174265ceb1c1fe1b91274316f1c3292de93
jsss-1 <3063494684@qq.com>
2024-06-08 19:53:27 +0800

    4-1-4

--- app/src/main/java/com/example/helloworld/jump/AActivity.java
+++ app/src/main/java/com/example/helloworld/jump/AActivity.java
@@ -3,7 +3,10 @@ package com.example.helloworld.jump;
 import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,14 +20,25 @@ import com.example.helloworld.util.ToastUtil;
 public class AActivity extends AppCompatActivity {
 
     private Button mBtnJump;
+    private Button mBtnJump2;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
-
+        Log.d("AActivity", "----onCreate----");
+        Log.d("AActivity", "taskid:" + getTaskId() + " ,hash:" + hashCode());
+        logtaskName();
         mBtnJump = (Button) findViewById(R.id.jump);
+        mBtnJump2 = (Button) findViewById(R.id.jump_2);
+        mBtnJump2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AActivity.this, AActivity.class);
+                startActivity(intent);
+            }
+        });
         mBtnJump.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,8 +48,8 @@ public class AActivity extends AppCompatActivity {
                 bundle.putString("name", "安卓");
                 bundle.putInt("number", 88);
                 intent.putExtras(bundle);
-//                startActivity(intent);
-                startActivityForResult(intent,0);
+                startActivity(intent);
+//                startActivityForResult(intent,0);
 
                 //显示2
 //                Intent intent=new Intent();
@@ -65,6 +79,23 @@ public class AActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Toast.makeText(AActivity.this,data.getExtras().getString("title"),Toast.LENGTH_LONG).show();
+        Toast.makeText(AActivity.this, data.getExtras().getString("title"), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("AActivity", "----onNewIntent----");
+        Log.d("AActivity", "taskid:" + getTaskId() + " ,hash:" + hashCode());
+        logtaskName();
+    }
+
+    private void logtaskName(){
+        try {
+            ActivityInfo info =getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+            Log.d("AActivity",info.taskAffinity);
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- app/src/main/java/com/example/helloworld/jump/BActivity.java
+++ app/src/main/java/com/example/helloworld/jump/BActivity.java
@@ -3,6 +3,8 @@ package com.example.helloworld.jump;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,22 +20,29 @@ public class BActivity extends AppCompatActivity {
 
     private TextView mTvTitle;
 
-    private Button mBtnFinish;
+    private Button mBtnFinish, mBtnJump;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
-
+        Log.d("BActivity", "----onCreate----");
+        Log.d("BActivity", "taskid:" + getTaskId() + " ,hash:" + hashCode());
+        logtaskName();
         mTvTitle = (TextView) findViewById(R.id.tv_title);
         mBtnFinish = (Button) findViewById(R.id.btn_finish);
+        mBtnJump = (Button) findViewById(R.id.btn_2);
+        mBtnJump.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BActivity.this, AActivity.class);
+                startActivity(intent);
+            }
+        });
         Bundle bundle = getIntent().getExtras();
         String name = bundle.getString("name");
         int number = bundle.getInt("number");
-        Log.e("B",bundle.toString());
-        Log.e("B",name);
-        Log.e("B",number+"");
 
         mTvTitle.setText(name + "," + number);
 
@@ -41,13 +50,30 @@ public class BActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                Bundle bundle1=new Bundle();
-                bundle1.putString("title","我回来了");
+                Bundle bundle1 = new Bundle();
+                bundle1.putString("title", "我回来了");
                 intent.putExtras(bundle1);
                 setResult(Activity.RESULT_OK, intent);
                 finish();
             }
         });
 
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("BActivity", "----onNewIntent----");
+        Log.d("BActivity", "taskid:" + getTaskId() + " ,hash:" + hashCode());
+        logtaskName();
+    }
+
+    private void logtaskName() {
+        try {
+            ActivityInfo info = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+            Log.d("BActivity", info.taskAffinity);
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
