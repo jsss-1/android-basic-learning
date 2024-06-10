56a9c41659634c9966afe85cc543c8a4a9542b9c
jsss-1 <3063494684@qq.com>
2024-06-10 16:13:21 +0800

    5-2

--- app/src/main/java/com/example/helloworld/EventActivity.java
+++ app/src/main/java/com/example/helloworld/EventActivity.java
@@ -2,23 +2,40 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.helloworld.util.ToastUtil;
+import com.example.helloworld.widget.MyButton;
 
 public class EventActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button mBtnEvent;
+    private MyButton btnMy;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
 
         mBtnEvent=(Button) findViewById(R.id.btn_event);
+        btnMy=(MyButton) findViewById(R.id.btn_my);
+        btnMy.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d("Listener","---onTouch---");
+                        break;
+                }
+                return false;
+            }
+        });
         //内部类实现
         mBtnEvent.setOnClickListener(new OnClick());
 //        //匿名内部类
@@ -64,5 +81,15 @@ public class EventActivity extends AppCompatActivity implements View.OnClickList
                 ToastUtil.showMsg(EventActivity.this,"click...");
                 break;
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log.d("Activity","---onTouchEvent---");
+                break;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/helloworld/widget/MyButton.java
@@ -0,0 +1,36 @@
+package com.example.helloworld.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class MyButton extends AppCompatButton {
+    public MyButton(Context context) {
+        super(context);
+    }
+
+    public MyButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyButton(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log.d("MyButton","---onTouchEvent---");
+                break;
+        }
+        return false;
+    }
+}
